2b30078bc32af30eab7406b93b2b84325d43bdcc
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-02-28 12:56:59 +0000

    tailwind v4 app added and setup

---
+++ apps/tailwind4/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, PluginOption } from "vite";
+import react from "@vitejs/plugin-react";
+// import csp from "vite-plugin-csp-guard";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react() as PluginOption,
+    tailwindcss() as PluginOption,
+    // csp({
+    //   dev: {
+    //     run: true,
+    //     outlierSupport: ["tailwind"],
+    //   },
+    //   build: {
+    //     sri: true,
+    //   },
+    // }),
+  ],
+  preview: {
+    port: 4003,
+  },
+  server: {
+    port: 3003,
+  },
+});
