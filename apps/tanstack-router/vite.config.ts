b1f8e1840b1b95ec53b8d70560dd29870c0aa5ac
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-03-29 23:04:53 +0000

    sri working

--- apps/tanstack-router/vite.config.ts
+++ apps/tanstack-router/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import csp from "vite-plugin-csp-guard";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
       routesDirectory: "./src/routes",
     }),
     react(),
+    csp({
+      build: {
+        sri: true,
+      },
+    }) as PluginOption,
   ],
   server: {
     port: 4022,
