5dfd05a13372bc854ec24ed06ad1551cc5fbf471
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2026-03-15 22:49:41 +0000

    add and update

--- playwright.config.ts
+++ playwright.config.ts
@@ -259,7 +259,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["list"],
+    ["html"],
+    ["json", { outputFile: "playwright-results/results.json" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
